cc0d646aec7dd4322f427f573ddc5071f31d2e3f
goelbenj <ben_goel@rogers.com>
2024-03-17 13:02:09 -0400

    Add firebase to react-app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebaseConfig from "./config";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+// import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 function App() {
+  // Initialize firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
   return (
     <div className="App">
       <header className="App-header">
